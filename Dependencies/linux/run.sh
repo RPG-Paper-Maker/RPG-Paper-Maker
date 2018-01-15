e07983c868ce75dfe9c78d35ea5a323f19128fbd
Wano <mmarie.laporte@gmail.com>
2018-01-15 18:24:09 +0100

    [urgent] Correcting linux bad dependencies

--- Dependencies/linux/run.sh
+++ Dependencies/linux/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 path="$0"
-SCRIPTPATH=${path%??????}
+SCRIPTPATH=${path%???????}
 export LD_LIBRARY_PATH="$SCRIPTPATH/libraries"
 export QT_QPA_PLATFORM_PLUGIN_PATH="$SCRIPTPATH/libraries/plugins/platforms"
+export QT_PLUGIN_PATH="$SCRIPTPATH/libraries/plugins"
 "$SCRIPTPATH/RPG-Paper-Maker"
