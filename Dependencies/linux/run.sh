7dfdf1601805023a3d39ef7654534525c99e6ef8
Wano <mmarie.laporte@gmail.com>
2018-01-22 23:15:59 +0100

    Removing engine dependencies

--- Dependencies/linux/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path="$0"
-SCRIPTPATH=${path%???????}
-export LD_LIBRARY_PATH="$SCRIPTPATH/libraries"
-export QT_QPA_PLATFORM_PLUGIN_PATH="$SCRIPTPATH/libraries/plugins/platforms"
-export QT_PLUGIN_PATH="$SCRIPTPATH/libraries/plugins"
-"$SCRIPTPATH/RPG-Paper-Maker"
