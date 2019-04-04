1c128052aab7397c7a65820c530166778c759427
Marie Laporte <mmarie.laporte@gmail.com>
2019-04-04 18:14:05 +0200

    Updating files license

--- Dialogs/Systems/dialogsystemwindowskin.cpp
+++ Dialogs/Systems/dialogsystemwindowskin.cpp
@@ -1,3 +1,13 @@
+/*
+    RPG Paper Maker Copyright (C) 2017-2019 Marie Laporte
+
+    RPG Paper Maker engine is under LGPL-3 license.
+
+    Commercial license for commercial use of your games:
+        https://creativecommons.org/licenses/by-nc/4.0/.
+
+    See more information here: http://rpg-paper-maker.com/index.php/downloads.
+*/
 #include "dialogsystemwindowskin.h"
 #include "ui_dialogsystemwindowskin.h"
 #include "rpm.h"
--- Dialogs/dialogvariables.h
+++ Dialogs/dialogvariables.h
@@ -1,20 +1,12 @@
 /*
     RPG Paper Maker Copyright (C) 2017-2019 Marie Laporte
 
-    This file is part of RPG Paper Maker.
+    RPG Paper Maker engine is under LGPL-3 license.
 
-    RPG Paper Maker is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Commercial license for commercial use of your games:
+        https://creativecommons.org/licenses/by-nc/4.0/.
 
-    RPG Paper Maker is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    See more information here: http://rpg-paper-maker.com/index.php/downloads.
 */
 
 #ifndef DIALOGVARIABLES_H
