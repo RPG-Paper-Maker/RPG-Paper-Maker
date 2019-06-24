7da3385083e1d9aea87d19261682493a1bc3d96f
Marie Laporte <mmarie.laporte@gmail.com>
2019-06-24 23:32:36 +0200

    [WIP] 3D objects importation: custom scale

--- Editor/MapEditor/object3dcustom.cpp
+++ Editor/MapEditor/object3dcustom.cpp
@@ -65,7 +65,8 @@ int Object3DCustomDatas::widthPixels() const {
     minimum = m_box.minimum();
     maximum = m_box.maximum();
 
-    return static_cast<int>(maximum.x() - minimum.x());
+    return static_cast<int>(static_cast<int>(maximum.x() - minimum.x()) *
+        m_datas->scale());
 }
 
 // -------------------------------------------------------
@@ -76,7 +77,8 @@ int Object3DCustomDatas::heightPixels() const {
     minimum = m_box.minimum();
     maximum = m_box.maximum();
 
-    return static_cast<int>(maximum.y() - minimum.y());
+    return static_cast<int>(static_cast<int>(maximum.y() - minimum.y()) *
+        m_datas->scale());
 }
 
 // -------------------------------------------------------
@@ -87,14 +89,18 @@ int Object3DCustomDatas::depthPixels() const {
     minimum = m_box.minimum();
     maximum = m_box.maximum();
 
-    return static_cast<int>(maximum.z() - minimum.z());
+    return static_cast<int>(static_cast<int>(maximum.z() - minimum.z()) *
+        m_datas->scale());
 }
 
 // -------------------------------------------------------
 
 void Object3DCustomDatas::getCenter(QVector3D &center) const {
-    QVector3D vec = m_box.center();
+    QVector3D vec;
+    float scale;
 
+    scale = static_cast<float>(m_datas->scale());
+    vec = m_box.center() * QVector3D(scale, scale, scale);
     center.setX(vec.x());
     center.setY(vec.y());
     center.setZ(vec.z());
@@ -119,11 +125,16 @@ int Object3DCustomDatas::minDistanceFromCenter() const {
 void Object3DCustomDatas::initializeVertices(QVector<Vertex> &vertices,
     QVector<GLuint> &indexes, Position &position, unsigned int &count)
 {
-    QVector3D positionOffset;
+    QVector3D positionOffset, size;
     SystemCustomShape *shape;
     QPair<int, int> index;
     int i, l, squareSize;
+    float scale;
 
+    scale = static_cast<float>(m_datas->scale());
+    size.setX(scale);
+    size.setY(scale);
+    size.setZ(scale);
     squareSize = RPM::get()->getSquareSize();
     shape = m_datas->obj();
     positionOffset.setX(position.x() * squareSize);
@@ -131,14 +142,14 @@ void Object3DCustomDatas::initializeVertices(QVector<Vertex> &vertices,
     positionOffset.setZ(position.z() * squareSize);
     for (i = 0, l = shape->facesCount(); i < l; i++) {
         index = shape->getFace(i);
-        vertices.append(Vertex(shape->getVertexAt(index.first) + positionOffset,
-            shape->getTextureAt(index.second)));
+        vertices.append(Vertex(shape->getVertexAt(index.first) * size +
+            positionOffset, shape->getTextureAt(index.second)));
         indexes.append(count++);
     }
 
     // Create collision box according to vertex positions
-    m_box = QBox3D(shape->minVertex() + positionOffset, shape->maxVertex() +
-        positionOffset);
+    m_box = QBox3D(shape->minVertex() * size + positionOffset, shape
+        ->maxVertex() * size + positionOffset);
 }
 
 // -------------------------------------------------------
