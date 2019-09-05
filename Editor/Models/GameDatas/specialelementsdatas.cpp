c11e5e315c850a579fb09b2a605acbd3e0173489
Marie Laporte <mmarie.laporte@gmail.com>
2019-09-05 17:53:20 +0200

    [FIX] Collisions on default 3D objects

--- Editor/Models/GameDatas/specialelementsdatas.cpp
+++ Editor/Models/GameDatas/specialelementsdatas.cpp
@@ -120,15 +120,15 @@ void SpecialElementsDatas::setDefaultAutotiles() {
 
 void SpecialElementsDatas::setDefaulObjects3D() { 
     this->addDefaultSpecial(new SystemObject3D(1, "Chest", ShapeKind::Box, -1,
-        -1, 1), m_modelObjects3D);
+        -1, 1, ObjectCollisionKind::Perfect), m_modelObjects3D);
     this->addDefaultSpecial(new SystemObject3D(2, "Cupboard", ShapeKind::Box, -1
-        ,  -1, 2, ObjectCollisionKind::None, -1, 1.0, 2, 0, 2, 0),
+        ,  -1, 2, ObjectCollisionKind::Perfect, -1, 1.0, 2, 0, 2, 0),
         m_modelObjects3D);
     this->addDefaultSpecial(new SystemObject3D(3, "Fridge", ShapeKind::Box, -1
-        ,  -1, 3, ObjectCollisionKind::None, -1, 1.0, 1, 0, 2, 0),
+        ,  -1, 3, ObjectCollisionKind::Perfect, -1, 1.0, 1, 0, 2, 0),
         m_modelObjects3D);
     this->addDefaultSpecial(new SystemObject3D(4, "Table", ShapeKind::Box, -1
-        ,  -1, 4, ObjectCollisionKind::None, -1, 1.0, 2, 0, 0, 50),
+        ,  -1, 4, ObjectCollisionKind::Perfect, -1, 1.0, 2, 0, 0, 50),
         m_modelObjects3D);
 }
 
