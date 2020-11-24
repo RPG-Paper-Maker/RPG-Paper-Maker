11d8afc715de77231cff429b1fe7146c2ad18875
Marie Laporte <mmarie.laporte@gmail.com>
2020-11-24 23:21:13 +0100

    [CLEAN] HTMLDelegate class

--- Editor/Utils/htmldelegate.cpp
+++ Editor/Utils/htmldelegate.cpp
@@ -1,34 +1,46 @@
+/*
+    RPG Paper Maker Copyright (C) 2017-2020 Wano
+
+    RPG Paper Maker engine is under proprietary license.
+    This source code is also copyrighted.
+
+    Use Commercial edition for commercial use of your games.
+    See RPG Paper Maker EULA here:
+        http://rpg-paper-maker.com/index.php/eula.
+*/
+
 #include <QPainter>
 #include <QTextDocument>
 #include <QAbstractTextDocumentLayout>
 #include "htmldelegate.h"
 #include "rpm.h"
 
-void HTMLDelegate::paint(QPainter* painter, const QStyleOptionViewItem & option,
+// -------------------------------------------------------
+//
+//  INTERMEDIARY FUNCTIONS
+//
+// -------------------------------------------------------
+
+void HTMLDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
     const QModelIndex &index) const
 {
     QStyleOptionViewItem options = option;
     initStyleOption(&options, index);
-
     painter->save();
-
     QTextDocument doc;
     doc.setHtml(options.text);
-
     options.text = "";
     options.widget->style()->drawControl(QStyle::CE_ItemViewItem, &options,
         painter);
 
-    // shift text right to make icon visible
+    // Shift text right to make icon visible
     QSize iconSize = options.icon.actualSize(options.rect.size());
     painter->translate(options.rect.left()+iconSize.width(), options.rect.top());
     QRect clip(0, 0, options.rect.width()+iconSize.width(), options.rect.height());
-
-    //doc.drawContents(painter, clip);
-
     painter->setClipRect(clip);
     QAbstractTextDocumentLayout::PaintContext ctx;
-    // set text color to red for selected item
+
+    // Set text color to red for selected item
     if (RPM::get()->engineSettings()->theme() == ThemeKind::Dark)
     {
         ctx.palette.setColor(QPalette::Text, QColor("white"));
@@ -36,21 +48,19 @@ void HTMLDelegate::paint(QPainter* painter, const QStyleOptionViewItem & option,
     {
         ctx.palette.setColor(QPalette::Text, QColor("black"));
     }
-    /*
-    if (option.state & QStyle::State_Selected)
-        ctx.palette.setColor(QPalette::Text, QColor("red"));*/
-
     ctx.clip = clip;
     doc.documentLayout()->draw(painter, ctx);
 
     painter->restore();
 }
 
-QSize HTMLDelegate::sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const
+// -------------------------------------------------------
+
+QSize HTMLDelegate::sizeHint(const QStyleOptionViewItem &option, const
+    QModelIndex &index) const
 {
     QStyleOptionViewItem options = option;
     initStyleOption(&options, index);
-
     QTextDocument doc;
     doc.setHtml(options.text);
     doc.setTextWidth(options.rect.width());
--- Editor/Utils/htmldelegate.h
+++ Editor/Utils/htmldelegate.h
@@ -1,13 +1,34 @@
+/*
+    RPG Paper Maker Copyright (C) 2017-2020 Wano
+
+    RPG Paper Maker engine is under proprietary license.
+    This source code is also copyrighted.
+
+    Use Commercial edition for commercial use of your games.
+    See RPG Paper Maker EULA here:
+        http://rpg-paper-maker.com/index.php/eula.
+*/
+
 #ifndef HTMLDELEGATE_H
 #define HTMLDELEGATE_H
 
 #include <QStyledItemDelegate>
 
+// -------------------------------------------------------
+//
+//  CLASS HTMLDelegate
+//
+//  A class used to have html tags inside treeview items.
+//
+// -------------------------------------------------------
+
 class HTMLDelegate : public QStyledItemDelegate
 {
 protected:
-    void paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
-    QSize sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const
+        QModelIndex &index ) const;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index)
+        const;
 };
 
 #endif // HTMLDELEGATE_H
