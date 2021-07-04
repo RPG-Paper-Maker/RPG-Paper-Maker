7a9c36a9fd56f74be6c18e84f7518905ce911b58
Marie Laporte <mmarie.laporte@gmail.com>
2021-07-04 20:40:28 +0200

    Update project version + build date

--- Editor/Models/project.cpp
+++ Editor/Models/project.cpp
@@ -19,8 +19,8 @@
 #include <QApplication>
 #include <QThread>
 
-const QString Project::ENGINE_VERSION = "1.8.1";
-const QString Project::LAST_BUILD_DATE = "June 25 2021";
+const QString Project::ENGINE_VERSION = "1.8.2";
+const QString Project::LAST_BUILD_DATE = "July 4 2021";
 const int Project::MAX_PROJECTS_NUMBER = 6;
 
 // -------------------------------------------------------
