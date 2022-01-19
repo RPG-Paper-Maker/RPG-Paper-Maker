91f093d7929eaa1a8867efd9910b7eab036a2806
Marie Laporte <mmarie.laporte@gmail.com>
2022-01-19 21:32:00 +0100

    Update copyright year

--- Editor/Enums/songkind.h
+++ Editor/Enums/songkind.h
@@ -1,5 +1,5 @@
 /*
-    RPG Paper Maker Copyright (C) 2017-2021 Wano
+    RPG Paper Maker Copyright (C) 2017-2022 Wano
 
     RPG Paper Maker engine is under proprietary license.
     This source code is also copyrighted.
