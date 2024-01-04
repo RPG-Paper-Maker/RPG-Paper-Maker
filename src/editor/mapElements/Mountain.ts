70b6a7eaad17c271eb8d989c6122c29ae3e4be0c
Marie Laporte <mmarie.laporte@gmail.com>
2024-01-04 13:35:48 +0100

    Update copyright 2024

--- src/editor/mapElements/Mountain.ts
+++ src/editor/mapElements/Mountain.ts
@@ -1,5 +1,5 @@
 /*
-    RPG Paper Maker Copyright (C) 2017-2023 Wano
+    RPG Paper Maker Copyright (C) 2017-2024 Wano
 
     RPG Paper Maker engine is under proprietary license.
     This source code is also copyrighted.
