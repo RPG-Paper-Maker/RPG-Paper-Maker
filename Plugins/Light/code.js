10ee5829eea1a4b861c543637e184c5fd817e130
Marie Laporte <mmarie.laporte@gmail.com>
2024-02-19 21:48:35 +0100

    [PLUGIN REQUEST] Light

---
+++ Plugins/Light/code.js
@@ -0,0 +1,248 @@
+import { RPM } from "../path.js"
+import { THREE } from "../../System/Globals.js";
+
+const pluginName = "Light";
+const inject = RPM.Manager.Plugins.inject;
+
+const onLoadID = RPM.Manager.Plugins.getParameter(pluginName, "Map load event ID");
+
+var lightList = [];
+var mapID = 0;
+
+setInterval(function ()
+{
+	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+		for (var i = 0; i < RPM.Scene.Map.current.scene.children.length; i++)
+			if (RPM.Scene.Map.current.scene.children[i].customDepthMaterial != null && !RPM.Scene.Map.current.scene.children[i].customDistanceMaterial)
+				RPM.Scene.Map.current.scene.children[i].customDistanceMaterial = new THREE.MeshDistanceMaterial({alphaTest: 0.5, map: RPM.Scene.Map.current.scene.children[i].customDepthMaterial.map});
+}, 48);
+
+setInterval(function ()
+{
+	if (!!RPM.Scene.Map.current && !RPM.Scene.Map.current.loading)
+	{
+		if (mapID != RPM.Scene.Map.current.id)
+		{
+			lightList = [];
+			mapID = RPM.Scene.Map.current.id;
+			RPM.Manager.Events.sendEventDetection(null, -1, false, onLoadID, [null]);
+		}
+		for (var i = 0; i < lightList.length; i++)
+		{
+			if (lightList[i].isDirectionalLight)
+			{
+				const x = lightList[i].extraStuff.x;
+				const y = lightList[i].extraStuff.y;
+				const z = lightList[i].extraStuff.z;
+				lightList[i].target.position.copy(RPM.Scene.Map.current.camera.targetPosition);
+				lightList[i].target.updateMatrixWorld();
+				lightList[i].position.set(x, y, z).multiplyScalar(RPM.Datas.Systems.SQUARE_SIZE * 20).add(RPM.Scene.Map.current.camera.targetPosition);
+				const d = Math.max(RPM.Datas.Systems.SQUARE_SIZE * RPM.Scene.Map.current.camera.distance / 10, 400);
+				if (d !== lightList[i].shadow.camera.right)
+				{
+					lightList[i].shadow.camera.left = -d;
+					lightList[i].shadow.camera.right = d;
+					lightList[i].shadow.camera.top = d;
+					lightList[i].shadow.camera.bottom = -d;
+					lightList[i].shadow.camera.updateProjectionMatrix();
+				}
+			}
+		}
+	}
+}, 16);
+
+RPM.Manager.GL.load = async function()
+{
+	const shader = RPM.Manager.Plugins.getParameter(pluginName, "Shaders directory path") + RPM.Manager.Plugins.getParameter(pluginName, "Shader");
+	const vert = await RPM.Common.IO.openFile(shader + ".vert");
+	const frag = await RPM.Common.IO.openFile(shader + ".frag");
+	RPM.Manager.GL.SHADER_FIX_VERTEX    = vert;
+	RPM.Manager.GL.SHADER_FIX_FRAGMENT  = frag;
+	RPM.Manager.GL.SHADER_FACE_VERTEX   = vert;
+	RPM.Manager.GL.SHADER_FACE_FRAGMENT = frag;
+}
+
+function enableCastShadows(mesh, enable)
+{
+	if (!mesh.isScene)
+	{
+		mesh.castShadow = enable;
+		mesh.receiveShadow = enable;
+	}
+	for (var i = 0; i < mesh.children.length; i++)
+		enableCastShadows(mesh.children[i], enable);
+}
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Remove all lights", () =>
+{
+	for (var i = 0; i < lightList.length; i++)
+	{
+		RPM.Scene.Map.current.scene.remove(lightList[i]);
+		lightList[i].dispose();
+	}
+	lightList = [];
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Remove default directional light", () =>
+{
+	for (var i = 0; i < RPM.Scene.Map.current.scene.children.length; i++)
+	{
+		if (RPM.Scene.Map.current.scene.children[i].isDirectionalLight && lightList.indexOf(RPM.Scene.Map.current.scene.children[i]) < 0)
+		{
+			RPM.Scene.Map.current.scene.remove(RPM.Scene.Map.current.scene.children[i]);
+			break;
+		}
+	}
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Remove light", (prop) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (!!light)
+	{
+		lightList.splice(lightList.indexOf(light), 1);
+		light.parent.remove(light);
+		light.dispose();
+	}
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set ambient light", (intensity, color) =>
+{
+	for (var i = 0; i < RPM.Scene.Map.current.scene.children.length; i++)
+	{
+		if (RPM.Scene.Map.current.scene.children[i].isAmbientLight && lightList.indexOf(RPM.Scene.Map.current.scene.children[i]) < 0)
+		{
+			RPM.Scene.Map.current.scene.children[i].intensity = intensity;
+			RPM.Scene.Map.current.scene.children[i].color = color.color;
+			break;
+		}
+	}
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Add hemisphere light", (prop, intensity, colorTop, colorBottom) =>
+{
+	const light = new THREE.HemisphereLight(colorBottom.color, colorTop.color, intensity);
+	if (prop > 0)
+		RPM.Core.ReactionInterpreter.currentObject.properties[prop] = light;
+	RPM.Scene.Map.current.scene.add(light);
+	lightList.push(light);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Add directional light", (prop, x, y, z, intensity, color, castShadow) =>
+{
+	const light = new THREE.DirectionalLight(color.color, intensity);
+	light.extraStuff = new THREE.Vector3(x, y, z).normalize();
+	light.castShadow = castShadow;
+	light.shadow.mapSize.width = 8192;
+	light.shadow.mapSize.height = 8192;
+	light.shadow.camera.far = RPM.Datas.Systems.SQUARE_SIZE * 350;
+	light.shadow.bias = -0.00002;
+	light.shadow.normalBias = 0.5;
+	if (prop > 0)
+		RPM.Core.ReactionInterpreter.currentObject.properties[prop] = light;
+	RPM.Scene.Map.current.scene.add(light);
+	lightList.push(light);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Add point light", (prop, id, x, y, z, intensity, color, radius, castShadow) =>
+{
+	const light = new THREE.PointLight(color.color, intensity);
+	light.shadow.bias = -0.001;
+	light.shadow.normalBias = 0.25;
+	light.distance = radius * RPM.Datas.Systems.SQUARE_SIZE;
+	light.position.set(x * RPM.Datas.Systems.SQUARE_SIZE, y * RPM.Datas.Systems.SQUARE_SIZE, z * RPM.Datas.Systems.SQUARE_SIZE);
+	light.castShadow = castShadow;
+	RPM.Core.MapObject.search(id, (result) =>
+	{
+		if (!!result)
+		{
+			result.object.mesh.add(light);
+			if (prop > 0)
+				RPM.Core.ReactionInterpreter.currentObject.properties[prop] = light;
+			lightList.push(light);
+		}
+	}, RPM.Core.ReactionInterpreter.currentObject);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Add spotlight", (prop, id, x, y, z, intensity, color, angle, distance, castShadow) =>
+{
+	const light = new THREE.SpotLight(color.color, intensity);
+	light.position.set(x * RPM.Datas.Systems.SQUARE_SIZE, y * RPM.Datas.Systems.SQUARE_SIZE, z * RPM.Datas.Systems.SQUARE_SIZE);
+	light.castShadow = castShadow;
+	light.angle = angle * Math.PI / 180.0;
+	light.shadow.bias = -0.00001;
+	light.shadow.normalBias = 0.75;
+	light.penumbra = 1.0;
+	light.distance = distance * RPM.Datas.Systems.SQUARE_SIZE;
+	RPM.Core.MapObject.search(id, (result) =>
+	{
+		if (!!result)
+		{
+			result.object.mesh.add(light);
+			light.target = result.object.mesh;
+			if (prop > 0)
+				RPM.Core.ReactionInterpreter.currentObject.properties[prop] = light;
+			lightList.push(light);
+		}
+	}, RPM.Core.ReactionInterpreter.currentObject);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set light color", (prop, color) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	light.color = color.color;
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set hemisphere light colors", (prop, colorTop, colorBottom) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (!light.isHemisphereLight)
+		return;
+	light.skyColor = colorBottom.color;
+	light.groundColor = colorTop.color;
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set light intensity", (prop, intensity) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	light.intensity = intensity;
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set light cast shadow", (prop, castShadow) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (light.isHemisphereLight)
+		return;
+	light.castShadow = castShadow;
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set light position", (prop, x, y, z) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (light.isHemisphereLight)
+		return;
+	if (light.isDirectionalLight)
+		light.extraStuff.set(x, y, z);
+	else
+		light.position.set(x * RPM.Datas.Systems.SQUARE_SIZE, y * RPM.Datas.Systems.SQUARE_SIZE, z * RPM.Datas.Systems.SQUARE_SIZE);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set spotlight target", (prop, id) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (!light.isSpotLight)
+		return;
+	RPM.Core.MapObject.search(id, (result) =>
+	{
+		if (!!result)
+			light.target = result.object.mesh;
+	}, RPM.Core.ReactionInterpreter.currentObject);
+});
+
+RPM.Manager.Plugins.registerCommand(pluginName, "Set spotlight angle", (prop, id) =>
+{
+	var light = RPM.Core.ReactionInterpreter.currentObject.properties[prop];
+	if (!light.isSpotLight)
+		return;
+	light.angle = angle * Math.PI / 180.0;
+});
---
+++ Plugins/Light/shaders/flat.frag
@@ -0,0 +1,197 @@
+#include <common>
+#include <bsdfs>
+#include <packing>
+#include <gradientmap_pars_fragment>
+#include <lights_pars_begin>
+#include <shadowmap_pars_fragment>
+
+uniform vec3 diffuse;
+uniform vec3 emissive;
+uniform float opacity;
+uniform float alpha_threshold;
+uniform vec4 colorD;
+uniform bool reverseH;
+uniform vec2 offset;
+uniform float repeat;
+uniform bool enableShadows;
+uniform sampler2D map;
+uniform vec3 specular;
+uniform float shininess;
+
+varying vec2 vUV;
+varying vec3 vNormal;
+varying vec3 fragPos;
+
+float flatten(in float value, in int layers)
+{
+	float threshold = 1.0 / float(layers);
+	float result = 1.0;
+	for (int i = 1; i < layers; i++)
+	{
+		if (value < float(i) * threshold)
+		{
+			result = float(i - 1) / float(layers - 1);
+			break;
+		}
+	}
+	return result;
+}
+
+#if NUM_DIR_LIGHT_SHADOWS > 0
+void getDirShadowMasks(inout float dirShadows[NUM_DIR_LIGHT_SHADOWS])
+{
+	DirectionalLightShadow directionalLight;
+	#pragma unroll_loop_start
+	for (int i = 0; i < NUM_DIR_LIGHT_SHADOWS; i++)
+	{
+		directionalLight = directionalLightShadows[i];
+		dirShadows[i] = receiveShadow ? getShadow(
+			directionalShadowMap[i],
+			directionalLight.shadowMapSize,
+			directionalLight.shadowBias,
+			directionalLight.shadowRadius,
+			vDirectionalShadowCoord[i]
+		) : 1.0;
+	}
+	#pragma unroll_loop_end
+}
+#endif
+
+#if NUM_SPOT_LIGHT_SHADOWS > 0
+void getSpotShadowMasks(inout float spotShadows[NUM_SPOT_LIGHT_SHADOWS])
+{
+	SpotLightShadow spotLight;
+	#pragma unroll_loop_start
+	for (int i = 0; i < NUM_SPOT_LIGHT_SHADOWS; i++)
+	{
+		spotLight = spotLightShadows[i];
+		spotShadows[i] = receiveShadow ? getShadow(
+			spotShadowMap[i],
+			spotLight.shadowMapSize,
+			spotLight.shadowBias,
+			spotLight.shadowRadius,
+			vSpotShadowCoord[i]
+		) : 1.0;
+	}
+	#pragma unroll_loop_end
+}
+#endif
+
+#if NUM_POINT_LIGHT_SHADOWS > 0
+void getPointShadowMasks(inout float pointShadows[NUM_POINT_LIGHT_SHADOWS])
+{
+	PointLightShadow pointLight;
+	#pragma unroll_loop_start
+	for (int i = 0; i < NUM_POINT_LIGHT_SHADOWS; i++)
+	{
+		pointLight = pointLightShadows[i];
+		pointShadows[i] = receiveShadow ? getPointShadow(
+			pointShadowMap[i],
+			pointLight.shadowMapSize,
+			pointLight.shadowBias,
+			pointLight.shadowRadius,
+			vPointShadowCoord[i],
+			pointLight.shadowCameraNear,
+			pointLight.shadowCameraFar
+		) : 1.0;
+	}
+	#pragma unroll_loop_end
+}
+#endif
+
+#if NUM_DIR_LIGHTS > 0
+vec4 getDirLight(const in int i, const in float shadow)
+{
+	vec3 color = directionalLights[i].color * shadow;
+	vec3 irradiance = getGradientIrradiance(normalize(vNormal), directionalLights[i].direction) * color;
+	return vec4(irradiance * BRDF_Lambert(diffuse), 0.0);
+}
+#endif
+
+#if NUM_SPOT_LIGHTS > 0
+vec4 getSpotLight(const in int i, const in float shadow)
+{
+	vec3 lightToFrag = spotLights[i].position - fragPos;
+	float dist = length(lightToFrag);
+	vec3 dir = normalize(lightToFrag);
+	float att = getSpotAttenuation(spotLights[i].coneCos, spotLights[i].penumbraCos, dot(dir, spotLights[i].direction));
+	att *= getDistanceAttenuation(dist, spotLights[i].distance, spotLights[i].decay);
+	vec3 color = spotLights[i].color * flatten(att, 5) * shadow;
+	vec3 irradiance = getGradientIrradiance(normalize(vNormal), dir) * color;
+	return vec4(irradiance * BRDF_Lambert(diffuse), 0.0);
+}
+#endif
+
+#if NUM_POINT_LIGHTS > 0
+vec4 getPointLight(const in int i, const in float shadow)
+{
+	vec3 lightToFrag = pointLights[i].position - fragPos;
+	float dist = length(lightToFrag);
+	vec3 dir = normalize(lightToFrag);
+	float att = getDistanceAttenuation(dist, pointLights[i].distance, pointLights[i].decay);
+	vec3 color = pointLights[i].color * flatten(att, 5) * shadow;
+	vec3 irradiance = getGradientIrradiance(normalize(vNormal), dir) * color;
+	return vec4(irradiance * BRDF_Lambert(diffuse), 0.0);
+}
+#endif
+
+void main()
+{
+	vec4 ambient = vec4(ambientLightColor.rgb / PI, 1.0); // maybe "/ PI" will have to be removed on more recent three.js revisions
+	vec2 coords = vec2(vUV.x + offset.x, vUV.y + offset.y);
+	vec2 pos = reverseH ? vec2(1.0 - coords.x, coords.y) : coords;
+	pos = vec2(pos.x - floor(pos.x), pos.y - floor(pos.y));
+	vec4 tex = texture2D(map, pos * repeat);
+	if (tex.a <= alpha_threshold)
+		discard;
+
+	#if NUM_DIR_LIGHT_SHADOWS > 0
+		float dirShadows[NUM_DIR_LIGHT_SHADOWS];
+		getDirShadowMasks(dirShadows);
+	#endif
+	#if NUM_SPOT_LIGHT_SHADOWS > 0
+		float spotShadows[NUM_SPOT_LIGHT_SHADOWS];
+		getSpotShadowMasks(spotShadows);
+	#endif
+	#if NUM_POINT_LIGHT_SHADOWS > 0
+		float pointShadows[NUM_POINT_LIGHT_SHADOWS];
+		getPointShadowMasks(pointShadows);
+	#endif
+
+	gl_FragColor = ambient + vec4(emissive, 0.0);
+	vec3 rgb = vec3(gl_FragColor.x + colorD.x, gl_FragColor.y + colorD.y, gl_FragColor.z + colorD.z);
+	vec3 intensity = vec3(dot(rgb, vec3(0.2125, 0.7154, 0.0721)));
+	gl_FragColor = vec4(mix(intensity, rgb, colorD.w), gl_FragColor.a);
+
+	#if NUM_HEMI_LIGHTS > 0
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_DIR_LIGHTS; i++)
+			gl_FragColor += vec4(getHemisphereLightIrradiance(hemisphereLights[i], vNormal), 0.0);
+		#pragma unroll_loop_end
+	#endif
+	#if NUM_DIR_LIGHTS > 0
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_DIR_LIGHTS; i++)
+			gl_FragColor += getDirLight(i, dirShadows[i]);
+		#pragma unroll_loop_end
+	#endif
+	#if NUM_SPOT_LIGHTS > 0
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_SPOT_LIGHTS; i++)
+			gl_FragColor += getSpotLight(i, spotShadows[i]);
+		#pragma unroll_loop_end
+	#endif
+	#if NUM_POINT_LIGHTS > 0
+		#pragma unroll_loop_start
+		for (int i = 0; i < NUM_POINT_LIGHTS; i++)
+			gl_FragColor += getPointLight(i, pointShadows[i]);
+		#pragma unroll_loop_end
+	#endif
+/*
+	#ifdef DECODE_VIDEO_TEXTURE
+		// inline sRGB decode (TODO: Remove this code when https://crbug.com/1256340 is solved)
+		tex = vec4(mix(pow(tex.rgb * 0.9478672986 + vec3(0.0521327014), vec3(2.4)), tex.rgb * 0.0773993808, vec3(lessThanEqual(tex.rgb, vec3(0.04045)))), tex.w);
+	#endif
+*/
+	gl_FragColor *= tex;
+}
