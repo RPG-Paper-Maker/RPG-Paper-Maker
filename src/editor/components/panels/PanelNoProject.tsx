89adee613b3591c9f8b44cfe082ba554c8c3c33c
Marie Laporte <mmarie.laporte@gmail.com>
2025-04-17 11:34:58 +0200

    Add link to guide to convert 2.0 to 3.0

--- src/editor/components/panels/PanelNoProject.tsx
+++ src/editor/components/panels/PanelNoProject.tsx
@@ -93,6 +93,13 @@ function PanelNoProject() {
 						<FaHandsHelping />
 						{t('contribute')}
 					</Button>
+					<a
+						href={'https://rpg-paper-maker.gitbook.io/rpg-paper-maker/others/convert-a-2.0-project-to-3.0'}
+						target='_blank'
+						rel='noreferrer'
+					>
+						{t('how.convert.project.2.0')}
+					</a>
 				</Flex>
 			</Flex>
 		</Flex>
