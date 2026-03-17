d4e292a82e4a586d3cdaf5b78b0d92fde1ae8884
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-03-17 20:33:07 +0100

    Fix plugins uppercase

--- Plugins/Fog/code.js
+++
@@ -1,9 +0,0 @@
-import { RPM } from "../path.js"
-import { THREE } from "../../System/Globals.js";
-
-const pluginName = "Fog";
-const inject = RPM.Manager.Plugins.inject;
-
-RPM.Manager.Plugins.registerCommand(pluginName, "Add fog", (intensity, color) => {
-    RPM.Scene.Map.current.scene.fog = new THREE.FogExp2(new THREE.Color(color.rgb), intensity);
-});
