4dc1f83fb79d1777be6bff83cb015b6ecfa20ae1
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-08-08 21:04:17 +0200

    Fix get commits scripts to avoid duplicates

--- scripts/get-commits.sh
+++ scripts/get-commits.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 cd ./ || exit 1
-git log master..develop --pretty=format:"%s"
-printf "\n"
-cd ../Game-Scripts || exit 1
-git log master..develop --pretty=format:"%s"
+declare -A commits
+
+print_unique_commits() {
+  local commit
+  while IFS= read -r commit; do
+    if [ -z "${commits[$commit]+x}" ]; then
+      commits[$commit]=1
+      printf "%s\n" "$commit"
+    fi
+  done
+}
+
+print_unique_commits < <(git log master..develop --pretty=format:"%s"; printf "\n")
+print_unique_commits < <(git -C ../Game-Scripts log master..develop --pretty=format:"%s"; printf "\n")
